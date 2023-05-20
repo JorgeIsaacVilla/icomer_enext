02b71ccfac9c9e85e2457ca74b2885642e0d5d0c
JorgeIsaacVilla <jvilla10@outlook.es>
2023-05-19 20:44:57 -0500

    mejora de aplicación

--- main.js
+++ main.js
@@ -73,10 +73,32 @@ function toggleCarritoAside () {
 }
 
 function openProductDetailAside () {
-    shoppingCartContainer.classList.add("inactive")
 
+
+    shoppingCartContainer.classList.add("inactive")
     productDetailContainer.classList.remove("inactive");
+
+
+
+    const productId = 1;
+    const filteredProducts = productList.filter(product => product.id === productId);
+    console.log("El producto encontrado:", filteredProducts[0]);
+
+    //const productCard = document.querySelectorAll(".product-card"); //aca leo el producto 
+    //const productId =  productCard.dataset.id; // estoy intentando sacar el ID del producto
+
+   // Buscar el producto correspondiente en productList utilizando el ID
+  //const product = productList.find(product => product.id === parseInt(productId, 10));
+
+    //console.log("El número de ID es:", product);
+
+
+    /*const filteredProducts = productList.filter(product => product.type === "ejercicio");*/
+    /*console.log(productList[0])
+    cardsContainer.innerHTML=""
+    renderProducts(filteredProducts);*/
 }
+
 
 function closeProductDetailAside () {
     productDetailContainer.classList.add("inactive");
@@ -84,245 +106,288 @@ function closeProductDetailAside () {
 
 const productList = [];
     productList.push({
+        id:1,
         name:"BK_model1",
         price: 2000000,
+        descri:"descripción del producto. calidad, costos, deportivo, vive al extremo enext",
         image: "https://fuelcarmagazine.com/wp-content/uploads/2022/10/Alpine-bicicleta-768x403.jpg",
         type: "bicicletas",
     });
     productList.push({
+        id:2,
         name:"BK_model2",
         price: 2000000,
+        descri:"descripción del producto. calidad, costos, deportivo, vive al extremo enext",
         image: "https://www.todomountainbike.net/images/articles/2014/peugeot-edl132-concept.jpg",
         type: "bicicletas",
     });
     productList.push({
+        id:3,
         name:"BK_model3",
         price: 2000000,
+        descri:"descripción del producto. calidad, costos, deportivo, vive al extremo enext",
         image: "https://img.automexico.com/2020/11/04/6Rw4sIlj/reevo-hubless-e-bike-2-cda6.jpg",
         type: "bicicletas",
     });
     productList.push({
+        id:4,
         name:"BK_model4",
         price: 2800000,
-        image: "https://mmedia.misrevistas.com/prospectivas/18570/imagen-1jpg-5909.jpg",
+        image: "https://2.bp.blogspot.com/-aHlxFNMxaeU/VDzl6u1jUQI/AAAAAAAACKI/u1DP9ArGnfs/s1600/FALCO%2BV1.jpg",
         type: "bicicletas",
     });
     productList.push({
+        id:5,
         name:"BK_model5",
         price: 2100000,
-        image: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2021/12/bicicleta-electrica-2554627.jpg?tf=3840x",
+        image: "https://www.lifeboxset.com/wp-content/uploads/2018/02/www.lifeboxset.com-screen-shot-2016-07-18-at-4.15.52-pm.png",
         type: "bicicletas",
     });
     productList.push({
+        id:6,
         name:"BK_model6",
         price: 1800000,
         image: "https://i.pinimg.com/originals/72/7a/48/727a48c1f3e4bafc2e2dd8b88954e82d.jpg",
         type: "bicicletas",
     });
     productList.push({
+        id:7,
         name:"BK_model7",
         price: 1700000,
-        image: "https://redlomas.com/wp-content/uploads/Screenshot_436.jpg",
+        image: "https://gourmetdemexico.com.mx/wp-content/uploads/2019/04/bicicletas-futuristas-bmw-Active-Hybrid.jpg",
         type: "bicicletas",
     });
     productList.push({
+        id:8,
         name:"BK_model8",
         price: 3850000,
-        image: "https://www.3dnatives.com/es/wp-content/uploads/sites/4/velo2.jpg",
+        image: "https://img.remediosdigitales.com/10eae0/650_1000_3026229-slide-136max/1366_2000.jpg",
         type: "bicicletas",
     });
     productList.push({
+        id:9,
         name:"Casco_VK1",
         price: 300000,
-        image: "https://http2.mlstatic.com/D_NQ_NP_778944-MCO52597959705_112022-W.jpg",
+        image: "https://www.motofichas.com/images/articulos/reportajes/cascos-integrales-deportivos-motogp/AGV-Pista-GP-RR.jpg",
         type: "accesorios",
     });
     productList.push({
+        id:10,
         name:"Casco_VK2",
         price: 300000,
-        image: "https://http2.mlstatic.com/D_NQ_NP_703548-MLM49514192816_032022-V.jpg",
+        image: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTBYMj4R_Kz3K5P_pWMq6pe4vBP_rwYK5MkuxQXeZrZWvYQ_66g3_Bka_IO34SntIo7PgRqzxWRZuD0DFy71kN2YtehvZOvs9gm-8yiD2Utdl46zlqeDS8w&usqp=CAE ",
         type: "accesorios",
     });
     productList.push({
+        id:11,
         name:"Casco_VK3",
-        price: 320000,
-        image: "https://http2.mlstatic.com/D_NQ_NP_748438-MLA46929047903_072021-V.jpg",
+        price: 180000,
+        image: "https://www.cascosconvinilo.com/web/wp-content/uploads/2019/06/6120002.jpg",
         type: "accesorios",
     });
     productList.push({
-        name:"Casco_VK4",
+        id:12,
+        name:"Gafas de sol",
         price: 350000,
-        image: "https://http2.mlstatic.com/D_NQ_NP_726025-MCO28038923890_082018-O.jpg",
+        image: "https://img.ltwebstatic.com/images3_pi/2021/07/06/162557073596157ae75aa44a7320d3cec4457eccba_thumbnail_600x.webp",
         type: "accesorios",
     });
     productList.push({
+        id:13,
         name:"Gafas_DP1",
         price: 180000,
-        image: "https://image.made-in-china.com/43f34j00HBQhAkuCZbrq/Ni-os-Miop-a-Piedad-Viper-Snowmobile-Dirt-Bike-Goggl-Harley-Adultos-Swim-Mayoreo-Motor-Cross-gafas-de-nataci-n-Anti-Niebla.jpg",
+        image: "https://img.ltwebstatic.com/images3_pi/2021/08/17/162916718048b8286e993fc327e41c4aba7466471a.webp",
         type: "accesorios",
     });
     productList.push({
+        id:14,
         name:"Gafas_DP2",
         price: 210000,
-        image: "https://img.ltwebstatic.com/images3_pi/2022/01/13/164203889575248ec822abc4132dc27e3ad6525de7_thumbnail_600x.webp",
+        image: "https://img.ltwebstatic.com/images3_pi/2021/09/13/163149887160a80ac6611beea64566ccedb75b1b97_thumbnail_600x.webp",
         type: "accesorios",
     });
     productList.push({
+        id:15,
         name:"Gafas_DP3",
         price: 230000,
-        image: "https://i.linio.com/p/42421c18be1e125180012c09c43c08be-product.jpg",
+        image: "https://img.ltwebstatic.com/images3_pi/2021/06/23/1624429624f5415ef129f86b376e01b9c7d2f1bcfd_thumbnail_600x.webp",
         type: "accesorios",
     });
     productList.push({
+        id:16,
         name:"GLV_DP1",
         price: 80000,
-        image: "https://go-greenbikes.com/wp-content/uploads/2021/10/8033637207995-001-750Wx750H.jpeg",
+        image: "https://w0.peakpx.com/wallpaper/754/480/HD-wallpaper-boxing-gloves-black-box-cat-fight-fighting-red-thanks-the.jpg",
         type: "accesorios",
     });
     productList.push({
+        id:17,
         name:"GLV_DP2",
         price: 60000,
-        image: "https://www.alkomprar.com/medias/8033637208121-001-750Wx750H?context=bWFzdGVyfGltYWdlc3wyMDk2NzZ8aW1hZ2UvanBlZ3xpbWFnZXMvaDBhL2gxOC85NzAzNjM2NzI5ODg2LmpwZ3w0Zjc1MTZmYjQzNDhjZjNhOTI0MjIyZWI2NjU0OGViZjUxOTMwOGJhZTliMzYyY2JmODk2YzczNzc2MzI4NzU1",
+        image: "https://images.motocard.com/eyJidWNrZXQiOiJtb3RvY2FyZC1ibG9nIiwia2V5IjoidXBsb2Fkc1wvMjAxOVwvMDZcLzIzMDkwOTExXC9sb2dvXzE3LTEyMDB4Njc1LmpwZyIsImVkaXRzIjp7IndlYnAiOnsicXVhbGl0eSI6ODV9LCJqcGVnIjp7InF1YWxpdHkiOjkxfSwicmVzaXplIjpbXX19",
         type: "accesorios",
     });
     productList.push({
+        id:18,
         name:"Cuatri Black",
         price: 14000000,
-        image: "https://http2.mlstatic.com/D_NQ_NP_977130-MCO69052779324_042023-O.jpg",
+        image: "https://media.pasionmovil.com/2021/12/Cyberquad-for-kids.jpg",
         type: "vehiculos",
     });
     productList.push({
+        id:19,
         name:"Cuatri Red",
         price: 18000000,
-        image: "https://m.media-amazon.com/images/I/61ZFh2OtMvL.jpg",
+        image: "https://cdn.shopify.com/s/files/1/0043/2078/8554/products/338800C350_B_2048x2048_c9a1643a-b838-42fa-ad12-db3b7a6223bb.jpg?v=1646760059",
         type: "vehiculos",
     });
     productList.push({
+        id:20,
         name:"Cuatri White",
         price: 24000000,
-        image: "https://bb1.vendomia-cdn.com/file/vendomia-assets/4/uploads/medium-nc-0fd756/2ff3d12e-quad-pantera-125cc-marcha-atras-naranja-frontal-izquierda1.jpg",
+        image: "https://cdn.shopify.com/s/files/1/2118/0861/products/cmy700-w_6_2000x.jpg?v=1498276403",
         type: "vehiculos",
     });
     productList.push({
+        id:21,
         name:"Trimoto 250cc Black",
         price: 18100000,
-        image: "https://motosnuevas.formulamoto.es/galeria/5282/Can_Am_Spyder_F3-S_10_Aniversario.jpg",
+        image: "https://www.diariomotor.com/imagenes/2011/11/lazareth-wazuma-04.jpg",
         type: "vehiculos",
     });
     productList.push({
-        name:"Trimoto 250cc green",
+        id:22,
+        name:"Trimoto 250cc blue",
         price: 18100000,
-        image: "https://p.turbosquid.com/ts-thumb/pG/5mHrFK/jg0SeROL/canamspyderrs_01/jpg/1532043011/600x600/fit_q87/3d3271e6db7594e42d060665c3655d32ab714fd4/canamspyderrs_01.jpg",
+        image: "https://gentedemoto.com/wp-content/uploads/2019/01/yamaha-niken.jpg",
         type: "vehiculos",
     });
     productList.push({
-        name:"Trimoto 500cc Family",
+        id:23,
+        name:"Trimoto 500cc",
         price: 26500000,
-        image: "https://can-am.brp.com/content/can-am-on-road/es_mx/modelos/spyder-rt/_jcr_content/root/modelteaser.coreimg.png/1673645246775/onrd-spy-my22-rt-sea-to-sky-mysterybluesatin-000g2nb00-studio-34fr-eu.png",
+        image: "https://delessencedansmesveines.com/wp-content/uploads/2017/12/z-DLEDMV-Lazareth-Wazuma-V8-1080x675.jpg",
         type: "vehiculos",
     });
     productList.push({
+        id:24,
         name:"Motocros V180cc",
         price: 8000000,
-        image: "https://cambiatumoto.cl/wp-content/uploads/2016/06/honda-crf110f.jpg",
+        image: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2021/04/niken_galeria1.jpg",
         type: "vehiculos",
     });
     productList.push({
+        id:25,
         name:"Motocros V120cc",
         price: 6000000,
-        image: "https://web2.fireboldweb.com/wp-content/uploads/2022/11/221120221669151525360_50_SX_FACTORY_1.png",
+        image: "https://publimotos.com/images/2021/julio-2021/11-julio/moto-tour/moto-tour-04.jpg",
         type: "vehiculos",
     });
     productList.push({
-        name:"Cuatri Vehiculo TT",
+        id:26,
+        name:"Vehículo familiar TT",
         price: 12400000,
-        image: "https://http2.mlstatic.com/D_NQ_NP_825525-MLU25461645119_032017-O.jpg",
+        image: "https://www.autofacil.es/wp-content/uploads/2021/09/mercedes_eqg-concept-21-300x188.jpg",
         type: "vehiculos",
     });
     productList.push({
+        id:27,
         name:"Cuatri moto 400cc",
         price: 16400000,
-        image: "https://http2.mlstatic.com/D_NQ_NP_920248-MLU30831008874_052019-W.jpg",
+        image: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/03/RAPTOR-90-2.jpg",
         type: "vehiculos",
     });
     productList.push({
+        id:28,
         name:"Cuatri moto 500cc",
         price: 18600000,
-        image: "https://4.bp.blogspot.com/-SpWVudhVqBk/ThZuANN7rsI/AAAAAAAAAPg/9InBcrMXIfA/s1600/can-am-2011-outlander-800r-x-mr-utility-efi-atv-black-yellow.jpg",
+        image: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2021/11/WOLVERINE-4.jpg",
         type: "vehiculos",
     });
     productList.push({
-        name:"Mancuernas 6P",
+        id:29,
+        name:"Mancuernas 80Kg",
         price: 380000,
-        image: "https://m.media-amazon.com/images/I/611SGK9Dl+L._AC_UF894,1000_QL80_.jpg",
+        image: "https://img.freepik.com/fotos-premium/mancuernas-entrenamiento-color-dorado-sobre-fondo-negro_103577-5193.jpg",
         type: "ejercicio",
     });
     productList.push({
-        name:"Mancuernas 8P",
+        id:30,
+        name:"Mancuernas 30Kg",
         price: 410000,
-        image: "https://m.media-amazon.com/images/I/51CPpGPyFEL._AC_.jpg",
+        image: "https://media.istockphoto.com/id/1078074278/es/foto/mancuernas-pesas-sobre-fondo-negro.jpg?s=170667a&w=0&k=20&c=FtUbs58IFxRafPOfztLEssNHhxMdYJUKOO6afmQRV5s=",
         type: "ejercicio",
     });
     productList.push({
-        name:"Mancuernas 4P",
+        id:31,
+        name:"Mancuernas 45Kg",
         price: 310000,
-        image: "https://m.media-amazon.com/images/I/61sDDvxZw6L._AC_SX522_.jpg",
+        image: "https://media.istockphoto.com/id/1078073730/es/foto/mancuernas-pesas-sobre-fondo-negro.jpg?s=170667a&w=0&k=20&c=-V4WvWqTfbG8_-VO-bRbezk8TGO2cKj9Hxm2Y4QPAnI=",
         type: "ejercicio",
     });
     productList.push({
-        name:"Mancuernas 2P",
+        id:32,
+        name:"Mancuernas 50Kg",
         price: 155000,
-        image: "https://m.media-amazon.com/images/I/41CYIiD9F6L.jpg",
+        image: "https://media.istockphoto.com/id/1078073872/es/foto/mancuernas-pesas-sobre-fondo-negro.jpg?s=170667a&w=0&k=20&c=V4dB1h61g6zEnP9PleeChH9pxCeR3ZawbKAcASOITFc=",
         type: "ejercicio",
     });
     productList.push({
+        id:33,
         name:"Ropar deportiva",
         price: 300000,
-        image: "https://i.pinimg.com/236x/e9/52/36/e95236323cc8061bff81ba8122fbe260.jpg",
+        image: "https://cf.shopee.com.mx/file/b51462cb27ce7a9550f6a60ad4bed513",
         type: "ropa",
     });
     productList.push({
+        id:34,
         name:"Ropar deportiva",
         price: 300000,
-        image: "https://http2.mlstatic.com/D_NQ_NP_731855-MLM51415215819_092022-O.jpg",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOpdssKgYhPJbUf9N_QdLuZwtisjqu0eVhbtDtbPSgYVG8dtgLnfQdEHisQrLMl_qq-jk&usqp=CAU",
         type: "ropa",
     });
     productList.push({
+        id:35,
         name:"Ropar deportiva",
         price: 300000,
-        image: "https://sc04.alicdn.com/kf/H5552b14c5cb64444b4bb03e2c38c78daR/230831424/H5552b14c5cb64444b4bb03e2c38c78daR.jpg",
+        image: "https://down-mx.img.susercontent.com/file/252955d1e62dbf88a37a1d4c284bd849",
         type: "ropa",
     });
     productList.push({
-        name:"zapatos",
+        id:36,
+        name:"Zapatos",
         price: 600000,
-        image: "https://http2.mlstatic.com/D_NQ_NP_746473-CBT68924132378_042023-V.jpg",
+        image: "https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2016/03/zapatillas-nike-HyperAdapt-1.0.jpg?fit=1600%2C900&quality=50&strip=all&ssl=1",
         type: "ropa",
     });
     productList.push({
-        name:"zapatos",
+        id:37,
+        name:"Zapatos",
         price: 800000,
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZCCmKT57IdE3Ms7pJRRtMnlK9DrLwnHObZpf3pfm0a2VcpSmA0ewZf1NmheyGrqkbt4w&usqp=CAU",
+        image: "https://www.eleconomista.com.mx/__export/1507851582885/sites/eleconomista/img/historico/tennis_nike_z.jpg_554688468.jpg",
         type: "ropa",
     });
     productList.push({
-        name:"zapatos",
+        id:38,
+        name:"Zapatos",
         price: 600000,
-        image: "https://m.media-amazon.com/images/I/61oJq0KTUmL._AC_SX466_.jpg",
+        image: "https://i.blogs.es/3d8f7c/nike-hyperadapt-white-gum/450_1000.jpg",
         type: "ropa",
     });
     productList.push({
-        name:"zapatos",
+        id:39,
+        name:"Zapatos",
         price: 850000,
-        image: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61+pf9DkBaL._AC_UY1000_.jpg",
+        image: "https://mott.pe/noticias/wp-content/uploads/2016/09/Luego-de-verlas-en-Volver-al-Futuro-II-finalmente-las-zapatillas-autoajustables-de-Nike-han-llegado-frontal.jpg",
         type: "ropa",
     });
-    productList.push({
-        name:"zapatos",
-        price: 640000,
-        image: "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/10950b0733af4968ab1daf4900b4bf61_9366/tenis-duramo-sl-2.0.jpg",
-        type: "ropa",
-    });
+        productList.push({
+            id:40,
+            name:"Zapatos",
+            price: 640000,
+            image: "https://mott.pe/noticias/wp-content/uploads/2016/09/Luego-de-verlas-en-Volver-al-Futuro-II-finalmente-las-zapatillas-autoajustables-de-Nike-han-llegado-led.jpg",
+            type: "ropa",
+        });
 
 function renderProducts(arr){
 
